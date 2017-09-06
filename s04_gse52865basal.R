780348b2b6c8aaa09b9608e13ef776415130ff65
atrassierra <31039134+atrassierra@users.noreply.github.com>
2017-09-06 19:16:06 +0200

    Update s04_gse52865basal.R

--- s04_gse52865basal.R
+++ s04_gse52865basal.R
@@ -1,14 +1,14 @@
 #########################################################################################
 ##                                                                                     ##
 ## s04_gse52865basal.R                                                                 ##
-## Script para la anotación de las DMRs, pondremos los genes a los que apuntan por     ##
-## el método del promedio y del valor absoluto. Además, prepararemos el input para el  ##
-## análisis de enriquecimiento funcional.                                              ##
+## Script para la anotaciÃ³n de las DMRs, pondremos los genes a los que apuntan por     ##
+## el mÃ©todo del promedio y del valor absoluto. AdemÃ¡s, prepararemos el input para el  ##
+## anÃ¡lisis de enriquecimiento funcional.                                              ##
 ## Autor: Antonio Manuel Trassierra Fresco                                             ##
 ##                                                                                     ##
 #########################################################################################
 
-# Cargamos las librerías necesarias
+# Cargamos las librerÃ­as necesarias
 
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(bumphunter)
@@ -18,57 +18,32 @@ setwd('/home/atrassierra/GSE52865')
 
 load('dmrs52865basal.RData')
 
-# Preparamos la anotación y matcheamos los genes
+# Preparamos la anotaciÃ³n y matcheamos los genes
 anno <- annotateTranscripts(TxDb.Hsapiens.UCSC.hg19.knownGene) # Preparamos el imput para matchGenes
 genes <- matchGenes(dmrs$table, subject = anno) # Mactheamos genes con sus dmrs
 
 data <- dmrs$table
-data <- cbind(data, genes$name) # Juntamos la informaciónn de los genes en una misma tabla
+data <- cbind(data, genes$name) # Juntamos la informaciÃ³nn de los genes en una misma tabla
 names(data)[length(names(data))] <- "genes" # Cambiamos el nombre a la columna de los genes
 
-datasorted <- data[order(-data$value),] # Ordenamos de mayor a menor nivel de metilación
+datasorted <- data[order(-data$value),] # Ordenamos de mayor a menor nivel de metilaciÃ³n
 
 # Escribimos el fichero
 
 write.table(x = datasorted, file = "dmrgen52865basal.txt", row.names = FALSE, sep = "\t")
 
-# Duplicados. Varias DMRs pueden apuntar a un mismo gen. Si esto ocurre, aunque no es lo más correcto,
-# promediaremos la señal de metilación porque hay veces que la señal apunta a más metilaciónn en casos o 
+# Duplicados. Varias DMRs pueden apuntar a un mismo gen. Si esto ocurre, aunque no es lo mÃ¡s correcto,
+# promediaremos la seÃ±al de metilaciÃ³n porque hay veces que la seÃ±al apunta a mÃ¡s metilaciÃ³nn en casos o 
 # en controles y no sabemos como interpretarlo.
 
 datasorted <- datasorted[, c("genes", "value")]
 
 # Promedio
 
-promedio <- duplicated(data$genes)
-table(promedio)
-genesdup <- data$genes[promedio]
-length(genesdup)
-
-ind1 <- data$genes %in% genesdup # Creamos una variable para saber cuantos duplicados hay en el data.frame
-table(ind1)
-
-dataprom <- data[ind1,] # Creamos un nuevo data.frame con los genes duplicados
-head(dataprom)
-dataprom <- dataprom[order(dataprom$genes),] # Con esto podemos ver los genes duplicados
-head(dataprom)
-dataprom <- dataprom[, c("genes", "value")]
-
-b <- aggregate(x = dataprom, by = list(genes = dataprom$genes), FUN = mean) # Promediamos las señales que apuntan a un mismo gen
-b <- b[, -2]
-
-genestotales <- data.frame(data$genes, data$value)
-colnames(genestotales) <- c("genes", "value")
-
-'%!in%' <- function(x, y)!('%in%'(x, y)) # Función not in creada
-
-ind2 <- genestotales$genes %!in% genesdup # Obtenemos los genes que no están duplicados
-genestotales <- genestotales[ind2,]
-table(genestotales$genes %in% dataprom$genes)
-
-final <- rbind(genestotales, b) # Juntamos todos los genes
-final <- final[order(-final$value),] # Ordenamos de mayor a menor nivel de metilación diferencial
+final <- datasorted %>% group_by(genes) %>% summarise(value = mean(value)) # Promediamos las DMR
+final <- final[order(-final$value),] # Ordenamos de mayor a menor nivel de metilaciÃ³n diferencial
 write.table(x = final, file = "genes52865basalprom.txt", row.names = FALSE, sep = "\t", quote = FALSE) # Input con el promedio para GSA
+save(final, file = "genes52865basalprom.RData")
 
 # Valor absoluto
 
